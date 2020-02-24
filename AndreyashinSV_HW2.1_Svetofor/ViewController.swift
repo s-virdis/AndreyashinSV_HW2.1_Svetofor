249244df957bf30c91e7866368f05b7d0f17c686
sa <sa@wrf.su>
2020-02-24 23:28:44 +0300

    Initial Commit

---
+++ AndreyashinSV_HW2.1_Svetofor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AndreyashinSV_HW2.1_Svetofor
+//
+//  Created by lastbyte on 24.02.2020.
+//  Copyright © 2020 lastbyte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
